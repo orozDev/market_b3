7b79e42655742b5b5dfc57b7658c950374ae6ded
Orozbek Zhenishbek uulu <oroz.jenishbek.2003@gmail.com>
2024-05-11 16:16:00 +0600

    rest api | register

--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from rest_framework.response import Response
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate
 from rest_framework.decorators import permission_classes
-from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 from api.filters import ProductFilter
 from api.permissions import IsSalesmanOrReadOnly, IsAdminUserOrReadOnly, IsSalesman
